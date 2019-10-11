c4d2e73e2eb03fed5fbc121856a7146bac465123
Thomas Campistron <irevoire@hotmail.fr>
2019-10-11 13:12:17 +0900

    fix the victory condition to handle the ex aequo possibilities

--- src/game.rs
+++ src/game.rs
@@ -4,35 +4,42 @@ use crate::grid::Cell;
 /// there is currently no winner.
 /// What if two persons win at the same time?
 /// This should not happens.
-pub fn finished(grid: &crate::grid::Grid) -> Cell {
-    let grid = &grid.grid;
+pub fn finished(grid: &Vec<Vec<Cell>>) -> Option<Cell> {
     // check if there is a line full
     if grid.iter().any(|l| l.iter().all(|c| c == &Cell::O)) {
-        return Cell::O;
+        return Some(Cell::O);
     }
     if grid.iter().any(|l| l.iter().all(|c| c == &Cell::X)) {
-        return Cell::X;
+        return Some(Cell::X);
     }
 
     // check if there is a column full
-    if grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0] {
-        return grid[0][0];
+    if grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0] && grid[0][0] != Cell::Empty {
+        return Some(grid[0][0]);
     }
-    if grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1] {
-        return grid[0][1];
+    if grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1] && grid[0][1] != Cell::Empty {
+        return Some(grid[0][1]);
     }
-    if grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2] {
-        return grid[0][2];
+    if grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2] && grid[0][2] != Cell::Empty {
+        return Some(grid[0][2]);
     }
 
     // check if there is a diagonal full
-    if grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] {
-        return grid[0][0];
+    if grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] && grid[0][0] != Cell::Empty {
+        return Some(grid[0][0]);
     }
-    if grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0] {
-        return grid[0][2];
+    if grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0] && grid[0][2] != Cell::Empty {
+        return Some(grid[0][2]);
     }
-    Cell::Empty
+
+    // now no one has win, is there a ex aequo?
+    // is the grid full?
+    if grid.iter().all(|l| l.iter().all(|c| c != &Cell::Empty)) {
+        return Some(Cell::Empty);
+    }
+
+    // still possible to play
+    None
 }
 
 #[cfg(test)]
@@ -42,87 +49,69 @@ mod tests {
 
     #[test]
     fn test_finish_line() {
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![X, X, X],
-                vec![Empty, Empty, Empty],
-                vec![Empty, Empty, Empty],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), X);
-        let g = crate::grid::Grid {
-            grid: vec![vec![X, O, X], vec![O, O, O], vec![X, X, O]],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), O);
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![Empty, Empty, Empty],
-                vec![Empty, Empty, Empty],
-                vec![Empty, Empty, Empty],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), Empty);
+        let g = vec![
+            vec![X, X, X],
+            vec![Empty, Empty, Empty],
+            vec![Empty, Empty, Empty],
+        ];
+        assert_eq!(finished(&g), Some(X));
+        let g = vec![vec![X, O, X], vec![O, O, O], vec![X, X, O]];
+        assert_eq!(finished(&g), Some(O));
     }
 
     #[test]
     fn test_finish_column() {
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![X, Empty, Empty],
-                vec![X, Empty, Empty],
-                vec![X, Empty, Empty],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), X);
-        let g = crate::grid::Grid {
-            grid: vec![vec![X, O, Empty], vec![X, O, X], vec![Empty, O, X]],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), O);
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![Empty, Empty, Empty],
-                vec![Empty, Empty, Empty],
-                vec![Empty, Empty, Empty],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), Empty);
+        let g = vec![
+            vec![X, Empty, Empty],
+            vec![X, Empty, Empty],
+            vec![X, Empty, Empty],
+        ];
+        assert_eq!(finished(&g), Some(X));
+        let g = vec![vec![X, O, Empty], vec![X, O, X], vec![Empty, O, X]];
+        assert_eq!(finished(&g), Some(O));
     }
 
     #[test]
     fn test_finish_diagonals() {
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![X, Empty, Empty],
-                vec![Empty, X, Empty],
-                vec![Empty, Empty, X],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), X);
-        let g = crate::grid::Grid {
-            grid: vec![vec![X, Empty, X], vec![Empty, X, Empty], vec![X, Empty, X]],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), X);
-        let g = crate::grid::Grid {
-            grid: vec![vec![O, X, X], vec![Empty, O, Empty], vec![X, X, O]],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), O);
-        let g = crate::grid::Grid {
-            grid: vec![
-                vec![Empty, Empty, Empty],
-                vec![Empty, X, Empty],
-                vec![Empty, Empty, X],
-            ],
-            cursor: (0, 0),
-        };
-        assert_eq!(finished(&g), Empty);
+        let g = vec![
+            vec![X, Empty, Empty],
+            vec![Empty, X, Empty],
+            vec![Empty, Empty, X],
+        ];
+        assert_eq!(finished(&g), Some(X));
+        let g = vec![vec![X, Empty, X], vec![Empty, X, Empty], vec![X, Empty, X]];
+        assert_eq!(finished(&g), Some(X));
+        let g = vec![vec![O, X, X], vec![Empty, O, Empty], vec![X, X, O]];
+        assert_eq!(finished(&g), Some(O));
+        let g = vec![vec![Empty, Empty, O], vec![Empty, O, X], vec![O, X, X]];
+        assert_eq!(finished(&g), Some(O));
+    }
+
+    #[test]
+    fn test_finished_ex_aequo() {
+        let g = vec![vec![X, O, X], vec![X, O, O], vec![O, X, X]];
+        assert_eq!(finished(&g), Some(Empty));
+    }
+
+    #[test]
+    fn test_finished_continue() {
+        let g = vec![
+            vec![Empty, Empty, Empty],
+            vec![Empty, X, Empty],
+            vec![Empty, Empty, X],
+        ];
+        assert_eq!(finished(&g), None);
+        let g = vec![
+            vec![O, Empty, Empty],
+            vec![Empty, Empty, Empty],
+            vec![Empty, Empty, X],
+        ];
+        assert_eq!(finished(&g), None);
+        let g = vec![
+            vec![Empty, Empty, Empty],
+            vec![Empty, Empty, Empty],
+            vec![Empty, Empty, Empty],
+        ];
+        assert_eq!(finished(&g), None);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod print;
 fn main() {
     let mut grid = grid::Grid::new();
 
-    while game::finished(&grid) == grid::Cell::Empty {
+    while game::finished(&grid.grid).is_none() {
         print::print(&grid);
         input::handle_input(&mut grid);
         ia::play(&mut grid);
@@ -16,9 +16,9 @@ fn main() {
     grid.cursor = (3, 3); // hack to hide the cursor
     print::print(&grid);
     print::end();
-    match game::finished(&grid) {
-        grid::Cell::O => println!("Victory"),
-        grid::Cell::X => println!(":("),
+    match game::finished(&grid.grid) {
+        Some(grid::Cell::O) => println!("Victory"),
+        Some(grid::Cell::X) => println!(":("),
         _ => println!("wtf"),
     }
 }
